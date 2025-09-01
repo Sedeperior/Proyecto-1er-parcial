b2342d05b331820bfd948718ecb21d643668e76c
Sedeperior <sdemianpr@gmail.com>
2025-08-31 23:28:16 -0600

    Add files via upload

---
+++ Proyecto Algoritmo.py
@@ -0,0 +1,4 @@
+#Calcular el tiempo con los ojos cerrados por el parpadeo
+N = float (input("Ingrese su edad en años: "))
+Resultado = N * 9.13125 + 1 - 1
+print (f"Haz pasado {Resultado} días parpandeando")
