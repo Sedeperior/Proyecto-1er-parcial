b9455093e67cf47fe9dc72a50ebcd37f09c6b4d9
Sedeperior <sdemianpr@gmail.com>
2025-09-14 22:06:46 -0600

    Add files via upload

--- Proyecto Algoritmo.py
+++ Proyecto Algoritmo.py
@@ -1,4 +1,10 @@
 #Calcular el tiempo con los ojos cerrados por el parpadeo
 N = float (input("Ingrese su edad en años: "))
+if N > 80 and N <= 120:
+    print (f"{N}! Eres bastante viejo" )
+elif N < 3:
+    print (f"{N}! Sabes leer?" )
+elif N > 120:
+    print (f"{N}!? Eso debe ser un record...")
 Resultado = N * 9.13125 + 1 - 1
 print (f"Haz pasado {Resultado} días parpandeando")
