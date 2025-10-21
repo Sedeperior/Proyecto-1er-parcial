0280403d9fee0fa3b43d3506e34fe9699229c0db
Sedeperior <sdemianpr@gmail.com>
2025-10-21 17:15:33 -0600

    Add files via upload
    Código terminado, favor de reevaluar de ser posible

--- Proyecto Algoritmo.py
+++ Proyecto Algoritmo.py
@@ -14,14 +14,14 @@ while True:
     actual = [N]
     if N > 80 and N <= 120:
         print (f"{N}! Eres bastante viejo" )
-    elif N < 3:
+    elif N <= 3:
         print (f"{N}! Sabes leer?" )
     elif N > 120:
         print (f"{N}!? Eso debe ser un record...")
     Resultado = N * 365.25 / 10 + 0.23
     actual.append(Resultado)
     edades.append(actual)
-    print (f"Haz pasado {Resultado} días parpandeando")
+    print (f"Haz pasado {Resultado:.2f} días parpandeando")
     print("\nRegistro de entradas y resultados:")
     for registro in edades:
         print(f"Edad: {registro[0]} años, con {registro[1]:.2f} días parpadeando")
