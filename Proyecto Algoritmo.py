c010f092780df18e55516e6612012dec4654f96c
Sedeperior <sdemianpr@gmail.com>
2025-10-21 16:45:30 -0600

    Add files via upload
    Código terminado, favor de reevaluar de ser posible

--- Proyecto Algoritmo.py
+++ Proyecto Algoritmo.py
@@ -1,9 +1,15 @@
 #Calcular el tiempo con los ojos cerrados por el parpadeo
 edades = []
+contador = 0
+limite = 5
 while True:
+    contador += 1
+    if contador > limite:
+        print("Máximo de entradas, gracias, se cerrará el programa")
+        break
     N = float(input("Ingrese su edad en años: "))
-    print("No creo, pon tu edad real")
-    if N > 150 or N < 3:
+    if N > 150 or N < 0:
+        print("No creo, pon tu edad real")
         continue
     actual = [N]
     if N > 80 and N <= 120:
@@ -19,4 +25,4 @@ while True:
     print("\nRegistro de entradas y resultados:")
     for registro in edades:
         print(f"Edad: {registro[0]} años, con {registro[1]:.2f} días parpadeando")
-    break
+    continue
