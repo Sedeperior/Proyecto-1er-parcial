baa8ecc80df872d776fd1285282805b112d74b4a
Sedeperior <sdemianpr@gmail.com>
2025-10-26 14:37:06 -0600

    Add files via upload

--- Proyecto Algoritmo.py
+++ Proyecto Algoritmo.py
@@ -1,4 +1,3 @@
-#Calcular el tiempo con los ojos cerrados por el parpadeo
 edades = []
 contador = 0
 limite = 5
@@ -7,22 +6,27 @@ while True:
     if contador > limite:
         print("Máximo de entradas, gracias, se cerrará el programa")
         break
-    N = float(input("Ingrese su edad en años: "))
-    if N > 150 or N < 0:
+    print("Ingrese su edad en: ")
+    años = float(input("Años: "))
+    meses = float(input("Meses: "))
+    días = float(input("Días: "))
+    resultado = (años * 365.25) + (meses * 30.27) + días
+    n = resultado / 365.25
+    parpa = resultado / 10
+    if n > 150 or n < 0:
         print("No creo, pon tu edad real")
         continue
-    actual = [N]
-    if N > 80 and N <= 120:
-        print (f"{N}! Eres bastante viejo" )
-    elif N <= 3:
-        print (f"{N}! Sabes leer?" )
-    elif N > 120:
-        print (f"{N}!? Eso debe ser un record...")
-    Resultado = N * 365.25 / 10 + 0.23
-    actual.append(Resultado)
+    actual = [n]
+    if años > 80 and años <= 120:
+        print (f"{años}! Eres bastante viejo" )
+    elif años <= 3:
+        print (f"{años}! Sabes leer?" )
+    elif años > 120:
+        print (f"{años}!? Eso debe ser un record...")
+    actual.append(parpa)
     edades.append(actual)
-    print (f"Haz pasado {Resultado:.2f} días parpandeando")
-    print("\nRegistro de entradas y resultados:")
+    print (f"Haz pasado {parpa:.2f} días parpandeando")
+    print("Registro de entradas y resultados:")
     for registro in edades:
-        print(f"Edad: {registro[0]} años, con {registro[1]:.2f} días parpadeando")
+        print(f"Edad: {registro[0]:.2f} años, con {registro[1]:.2f} días parpadeando")
     continue
